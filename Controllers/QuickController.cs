2f194c769f457eedd06821d839ec04f5f89303c4
Quinntyne Brown <Quinntyne.Brown@shawmedia.ca>
2016-04-26 12:19:35 -0400

    initial code

---
+++ Controllers/QuickController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+namespace quick.Controllers
+{
+    [RoutePrefix("api/quick")]
+    public class QuickController : ApiController
+    {
+        [Route("get")]
+        public IHttpActionResult Get()
+        {
+            return Ok(new { Name = "Quick" });
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(quick.Startup))]
+
+namespace quick
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configure(config => {                
+                var jSettings = new JsonSerializerSettings();
+                jSettings.Formatting = Formatting.Indented;
+                jSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                config.Formatters.Remove(config.Formatters.XmlFormatter);
+                config.Formatters.JsonFormatter.SerializerSettings = jSettings;
+            });
+
+        }
+    }
+}
